5c9641f0542c2c8c99db2ed6abf2717dff41a8db
Aw Man <brambilladaniele@icloud.com>
2023-09-09 22:47:12 +0200

    "if something doesn't work, just delete it and refactor." -me

--- src/main/java/net/awman/advancedtp/AdvancedTpClient.java
+++ src/main/java/net/awman/advancedtp/AdvancedTpClient.java
@@ -1,11 +1,10 @@
 package net.awman.advancedtp;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.awman.advancedtp.util.ModModelPredicateProvider;
 
 public class AdvancedTpClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        ModModelPredicateProvider.registerModModels();
+        AdvancedTp.LOGGER.debug("Initializing " + AdvancedTp.MOD_ID + " client...");
     }
 }
