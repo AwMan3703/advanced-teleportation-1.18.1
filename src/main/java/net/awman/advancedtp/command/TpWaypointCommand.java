2157172936f11c8a2395c49513abc5f1723bf0a0
Aw Man <brambilladaniele@icloud.com>
2023-09-10 10:22:44 +0200

    documented TpWaypointCommand.java

--- src/main/java/net/awman/advancedtp/command/TpWaypointCommand.java
+++ src/main/java/net/awman/advancedtp/command/TpWaypointCommand.java
@@ -4,16 +4,10 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import net.awman.advancedtp.AdvancedTp;
-import net.awman.advancedtp.AdvancedTpClient;
 import net.awman.advancedtp.util.IEntityDataSaver;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.network.MessageType;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.LiteralText;
-
-import java.util.UUID;
 
 import static com.mojang.brigadier.arguments.StringArgumentType.getString;
 
@@ -26,25 +20,37 @@ public class TpWaypointCommand {
     }
 
     private static int run(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
-        context.getSource().sendFeedback(new LiteralText("Command disabled"), true);
-        /*IEntityDataSaver player = (IEntityDataSaver) context.getSource().getPlayer();
+        // Get the player handle, to read their persistent data
+        IEntityDataSaver player = (IEntityDataSaver) context.getSource().getPlayer();
 
+        // Get the waypoint id from the command
         final String waypointId = getString(context, "waypoint_id");
 
-        // not 0 means it contains SOMETHING
+        // Get the waypoint position from the player's persistentData.
+        // This is achieved by requesting an IntArray, with the waypoint_id as key
         int[] waypointCoords = player.getPersistentData().getIntArray(waypointId);
 
-        if(waypointCoords.length != 0) {
+        // If the waypoint coordinates' length is > 0 means it contains SOMETHING
+        if(waypointCoords.length > 0) {
+
+            // Get the waypoint's coordinates
             int[] playerPos = player.getPersistentData().getIntArray(waypointId);
+
+            // Teleport the player there
             context.getSource().getPlayer().requestTeleport(playerPos[0], playerPos[1], playerPos[2]);
 
+            // Write feedback in chat ("Teleported to <waypoint id>")
             context.getSource().sendFeedback(new LiteralText("Teleported to " + waypointId + "!"), true);
+
+            // Return 1 (completed without errors)
             return 1;
         }
         else {
+            // If the waypoint coordinates' length is 0 (a.k.a. doesn't contain anything), inform the player
             context.getSource().sendFeedback(new LiteralText("That waypoint doesn't exist!"), true);
+
+            // Return -1 (completed with errors)
             return -1;
-        }*/
-        return 1;
+        }
     }
 }
